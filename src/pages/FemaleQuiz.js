aadff604ca4239de5d00a8e1e48cca6de6321fd2
Tomiwa <mogbosultimate@gmail.com>
2021-09-28 19:04:15 +0100

    Added 2sure logo and fotter social links

--- src/pages/FemaleQuiz.js
+++ src/pages/FemaleQuiz.js
@@ -148,12 +148,21 @@ export default function FemaleQuiz() {
       style={{ backgroundImage: "url(../images/question-bg.jpg)" }}
     >
       <header className="header">
-        <div class="container">
-          <Link to="/" className="logo">
-            <img src="../images/logo.png" />
-          </Link>
-        </div>
-      </header>
+          <div class="container">
+          <div class="row">
+                <div class="col-6">
+                <Link to="/" className = "logo">
+                    <img src="../images/2sure-logo.png" style={{height: "80px"}}/>
+                </Link>
+                </div>
+                <div class="col-6 text-right">
+                <Link to="/" className = "logo">
+                    <img src="../images/logo.svg"/>
+                </Link>
+                </div>
+            </div>
+          </div>
+        </header>
 
       <div class="wrapper">
         <div class="container">
@@ -316,13 +325,44 @@ export default function FemaleQuiz() {
 
       <div class="footer">
           <div class="container">
-            <Link to="/privacy" style={{color:"white"}}>
-            <div class="col-md-12">
-              <span>TERMS OF USE/ PRIVACY</span>
+            <div class="row">
+              <div class="col-md-6">
+              <Link to="/privacy" style={{color:"white"}}>
+                <div class="col-md-12">
+                  <span>TERMS OF USE/ PRIVACY</span>
+                </div>
+              </Link>
+              </div>
+
+              <div class="col-md-6 text-right">
+              <a
+            target="_blank"
+            style={{color:"white"}}
+            rel="noopener noreferrer"
+            className = "share-button mr-3"
+            href="https://www.facebook.com/2SureNigeria/">
+            <i class="fab fa-facebook-square"></i>
+          </a>
+            <a
+            target="_blank"
+            style={{color:"white"}}
+            rel="noopener noreferrer"
+            className = "share-button mr-3"
+            href="https://www.instagram.com/2sureng/">
+            <i class="fab fa-instagram"></i>
+          </a>
+              <a                         
+            target="_blank"
+            style={{color:"white"}}
+            rel="noopener noreferrer"
+            className = "share-button mr-3" href="https://www.twitter.com/2SureNG/"><i class="fab fa-twitter-square"></i></a> 
+             
+              </div>
             </div>
-            </Link>
+            
           </div>
         </div>
+
     </div>
   );
 }
